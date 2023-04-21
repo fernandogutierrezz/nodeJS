cf5732212b4e77d0ed939c8437212b34c4728277
Fernando Gutierrez <110203103+fernandogutierrezz@users.noreply.github.com>
2023-04-21 08:38:05 -0400

    curso node

---
+++ 2_fundamentos/2_export_import/index.mjs
@@ -0,0 +1,3 @@
+import soma from './meu_modulo.mjs' //importando o modulo
+
+soma(3,10) //utilizando o modulo
---
+++ 2_fundamentos/3_core_module/index.js
@@ -0,0 +1,5 @@
+const path = require('path') //busca modulo
+
+const extension = path.extname('arquivo.pdf') //passando um parâmetro para o modulo "EXTNAME"
+
+console.log(extension) //imprimindo o resultado na tela!
